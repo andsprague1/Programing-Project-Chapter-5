bd7273777c823708d3a32f7119eaeabc44ce9a55
andsprague1 <andsprague1@gmail.com>
2026-04-01 17:54:29 -0400

    Comit C to F

--- Programing Project Chapter 5/Programing Project Chapter 5.cpp
+++ Programing Project Chapter 5/Programing Project Chapter 5.cpp
@@ -1,13 +1,27 @@
 // Programing Project Chapter 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Displaces Celsius and Fharahsf temp
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "This program output the Celsius and Fahriodfnsoa tempuratures \n";
+    cout << "For 0 to 20\n";
+    cout << "C         F\n";
+    cout << "_________________\n";
+
+    //loop through values
+    for (int i = 0; i <= 20; i++)
+    {
+        //calculate F tempurature
+        double ftemp = i * 9 / 5.0 + 32;
+        cout << setprecision(2) << fixed << showpoint;
+        cout << "C:"<<setw(4) << i << setw(6) << "| F:" << setw(4) << ftemp << endl;
+    }
+
 
     return 0;
 }
