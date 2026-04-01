35daaaab2e5a0c56e3eb04e380cbc157babaf5e0
andsprague1 <andsprague1@gmail.com>
2026-04-01 17:34:49 -0400

    added using namespace statement

--- Programing Project Chapter 5/Programing Project Chapter 5.cpp
+++ Programing Project Chapter 5/Programing Project Chapter 5.cpp
@@ -3,9 +3,13 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
